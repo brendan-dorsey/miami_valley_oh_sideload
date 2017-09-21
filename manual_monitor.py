719f16918f3fba2703bc3f2742cce79076dcff9e
Brendan Dorsey <beeg.dorsey@gmail.com>
2017-09-21 12:52:18 -0700

    Add documentation.

--- manual_monitor.py
+++ manual_monitor.py
@@ -20,62 +20,37 @@ def main():
     page = response.content
     soup = BeautifulSoup(page, "html.parser")
 
+    # Collect all data rows from display table
     data_rows = soup.findAll("tr", { "class" : "dxgvDataRow" })
 
-    # print data_rows
-    # print len(data_rows)
-    # print type(data_rows)
-
-    # for i, row in enumerate(data_rows):
-    #     print i + 1
-    #     print type(row)
-    #     print row.contents
-    #     print "length: ", len(row.contents)
-    #     print ""
-    #     for i, tag in enumerate(row.contents):
-    #         print i
-    #         print tag.string
-    #         print ""
-    #     break
-
+    # Loop through data rows to build output array
     incidents_array = []
     for i, row in enumerate(data_rows):
         incident = {}
         row_data = row.contents
+
+        # Control to ensure only filled rows are parsed
         if len(row_data) < 9:
             continue
 
+        # Parse row data by position (html tags are common to all fields) and
+        # update incident information with relevant data
         incident["timestamp"] = row_data[2].string
         incident["id"] = "montgomery_county_oh_pd_" + row_data[3].string
         incident["headline"] = row_data[4].string
+
+        # Logic for interpreting location coding
         location = row_data[6].string[:-4] + ", OH"
         location = location.replace("/", " and ")
         location = location.replace("PK", "PIKE")
         location = location.replace("BL", "BLVD")
         incident["location"] = location
 
+        # Add incident to output array
         incidents_array.append(incident)
 
-    # print incidents_array
-
-    for item in incidents_array:
-        print item["location"]
-
-    print incidents_array
-
-
-
-
-
-
-
-def single_query(link, payload):
-    response = requests.get(link, params=payload)
-    if response.status_code != 200:
-        print 'WARNING', response.status_code
-    else:
-        return response.json()
+    return incidents_array
 
 
 if __name__ == '__main__':
-    main()
+    print main()
